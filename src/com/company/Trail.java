8044d5b4d67ad99b96c4b70de71e1d87f88ccbc7
BT_1N3_17 <ctam9591@bths.edu>
2018-12-21 11:26:57 -0500

    update

--- src/com/company/Trail.java
+++ src/com/company/Trail.java
@@ -1,9 +1,8 @@
 package com.company;
 
-public class Trail extends Runner {
-
+public class Trail
+{
     private int[] markers = {100,150,105,120,90,80,51,75,75,70,80,90,100};
-
 
     public boolean isLevelTrailSegment(int start, int end)
     {
@@ -12,24 +11,21 @@ public class Trail extends Runner {
         for (int i = start + 1; i <= end; i++)
         {
             if (min > this.markers[i])
-            {
                 min = this.markers[i];
-            }
             if (max < this.markers[i])
-            {
                 max = this.markers[i];
-            }
         }
         return ((max - min) <= 10);
     }
 
 
-    public boolean isDifficult() {
+    public boolean isDifficult()
+    {
         int numChanges = 0;
-        for (int i = 0; i < this.markers.length - 1; i++) {
-            if (Math.abs(this.markers[i] - this.markers[i + 1]) >= 30) {
+        for (int i = 0; i < this.markers.length - 1; i++)
+        {
+            if (Math.abs(this.markers[i] - this.markers[i + 1]) >= 30)
                 numChanges++;
-            }
         }
         return (numChanges >= 3);
     }
